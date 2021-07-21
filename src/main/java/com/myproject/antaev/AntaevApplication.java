f3cb99edefcb0f4d4232dfe1decf44eb868a4dd9
slied256 <slied256@gmail.com>
2021-07-21 20:29:52 +0400

    add dependencies

---
+++ src/main/java/com/myproject/antaev/AntaevApplication.java
@@ -0,0 +1,14 @@
+package com.myproject.antaev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntaevApplication {
+    public AntaevApplication() {
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntaevApplication.class, args);
+    }
+}
