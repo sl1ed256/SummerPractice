ed283403aa7ef7432ce209d90466f89cfaf83258
slied256 <slied256@gmail.com>
2021-08-05 00:46:10 +0400

    Add change for project and task

--- src/main/java/com/myproject/antaev/repository/TaskRepository.java
+++ src/main/java/com/myproject/antaev/repository/TaskRepository.java
@@ -1,10 +1,19 @@
 package com.myproject.antaev.repository;
 
 import com.myproject.antaev.entity.TaskEntity;
+import com.myproject.antaev.rest.dataType.StatusTask;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface TaskRepository extends JpaRepository<TaskEntity, Integer> {
+    //количество задач проекта в статусе
+    @Query(value = "select count(p.id) from TaskEntity t " +
+            "join t.release r " +
+            "join r.project p " +
+            "where p.id = :id and " +
+            "t.status != :status")
+    Long countTasksInProcessByProjectId(int id, StatusTask status);
 }
